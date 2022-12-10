f631b15df5a50d3650d05939ca63db774c34bdf4
Fábio Henning Ferreira <fabioh.f@hotmail.com>
2022-12-09 23:22:06 -0300

    Exemplos

---
+++ Semana 02/Aula 03/index.js
@@ -0,0 +1,27 @@
+/*Caro Fulano(a)!
+Você está convidado(a) para o casamento de Beltrano(a) e 
+Ciclano(a), a ser realizado no dia 05/12/2022, às 16 horas.
+Contamos com a sua presença!
+Atenciosamente,
+os noivo(a)s */
+
+var convidado = 'Fábio';
+var noivos = 'João e Maria';
+var dataFormatada = '05/12/2022, às 16 horas';
+
+var texto =
+  'Caro(a) ' +
+  convidado +
+  '!\n\nVocê está convidado(a) para o casamento de ' +
+  noivos +
+  ', a ser realizado no dia ' +
+  dataFormatada +
+  '.\n\nContamos com a sua presença!';
+
+console.log(texto);
+
+// String
+
+var nome = 'teste';
+
+// Number
